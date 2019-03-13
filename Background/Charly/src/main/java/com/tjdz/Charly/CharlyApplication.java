3ac6fe881a25cce6fe27b743d88bdf468f81f946
JimmyZhao <JimCoffey@users.noreply.github.com>
2019-03-13 15:15:34 +0800

    upload background service

---
+++ Background/Charly/src/main/java/com/tjdz/Charly/CharlyApplication.java
@@ -0,0 +1,13 @@
+package com.tjdz.Charly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharlyApplication.class, args);
+	}
+
+}
---
+++ Background/Charly/src/test/java/com/tjdz/Charly/CharlyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tjdz.Charly;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CharlyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
